290b7df7302dbb22f143919e71ebb5edc77d4816
Mujeeb218353 <139048778+Mujeeb218353@users.noreply.github.com>
2024-03-08 18:56:26 +0500

    first commit

---
+++ src/components/form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Loader from "./Loader";
+
+function Form() {
+    return (
+        <div>
+            <Loader/>
+            <h1>Hello from About</h1>
+        </div>
+    );
+}
+
+export default Form;
